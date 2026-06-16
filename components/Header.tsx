3f78273d13bf632ad1114fb69921022ee98d52d3
sehrishnadeem665 <sehrishnadeem665@gmail.com>
2026-06-16 17:31:50 +0500

    afA

--- components/Header.tsx
+++ components/Header.tsx
@@ -28,13 +28,13 @@ export default function Header() {
   return (
     <>
       <header className="sticky top-0 z-[40] bg-white/90 backdrop-blur-md border-b border-gray-200 shadow-sm">
-        <div className="container mx-auto px-6 py-4">
+        <div className="container mx-auto px-6 py-2">
           <div className="flex items-center justify-between">
 
             {/* LOGO */}
             <Link href="/" className="flex items-center gap-2">
               <div className="flex items-center gap-1">
-                <span className="text-lg sm:text-xl md:text-2xl font-extrabold bg-gradient-to-r from-gray-900 via-blue-600 to-blue-400 bg-clip-text text-transparent">
+                <span className="text-2xl sm:text-2xl md:text-3xl font-extrabold bg-gradient-to-r from-gray-900 via-blue-600 to-blue-400 bg-clip-text text-transparent">
                   Car Readers
                 </span>
               
@@ -91,7 +91,7 @@ export default function Header() {
         <div className="fixed inset-0 z-[100] bg-white p-4 sm:p-6">
           <div className="flex justify-between items-center mb-6">
             <div className="flex items-center gap-1">
-              <span className="text-lg font-extrabold bg-gradient-to-r from-gray-900 via-blue-600 to-blue-400 bg-clip-text text-transparent">
+              <span className="text-2xl font-extrabold bg-gradient-to-r from-gray-900 via-blue-600 to-blue-400 bg-clip-text text-transparent">
                 Car Readers
               </span>
             
